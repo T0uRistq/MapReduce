3a5576675f8d28d55627856527aa0467884eef2f
T0uRistq <artur.ilyasov.2000@mail.ru>
2023-05-14 23:49:47 +0500

    remove threads

--- mapreduce.hpp
+++
@@ -1,6 +0,0 @@
-#include "schedule.hpp"
-
-void mapreduce(void (*mf)(string, string), void (*rf)(string, vector<string>), vector<string> paths) {
-    schedule(mf, paths, 10, 0); // schedule workers for map
-    schedule(rf, paths, 10, 1); // schedule workers for reduce
-}
